1dc873bd36b07ca4a1e21c897f2dd2dd09a7b331
Andru Alarcon <idazhdz@gmail.com>
2025-06-11 02:08:40 -0400

    Primer commit gateway

---
+++ src/main/java/com/perfulandia/perfulandia_api_gateway/PerfulandiaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/perfulandia_api_gateway/PerfulandiaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
